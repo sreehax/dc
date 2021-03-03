4da3a770bee2b2783ea3bc68c47655b75c868230
Marco Cetica <ceticamarco@gmail.com>
2021-03-04 00:43:57 +0100

    Added cli parsing, file parsing and refactored code

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # dc
+`dc` is a RPN calculator written in Rust that aims to be the UNIX's dc clone. This implementation of `dc`
+does supports only a proper subset of the `dc`'s features(see below for a detailed list).  
+
+## Installation
+Since `dc` is written in Rust(at least mine), you need to install a 
+[Rust compiler](https://www.rust-lang.org/) in order to build this tool.
+Then you can just type:
+
+```bash
+cargo build --release
+```
+You will find the executable file under `target/release/dc`.
+
+## Usage
+`dc` uses [reverse polish notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation) to evaluate the expressions. For a general
+list of options you can run `dc -h`:
+
+```
+dc - RPN Desk calculator 0.1.0
+Marco C. <ceticamarco@gmail.com>
+UNIX's dc clone
+
+USAGE:
+    dc [OPTIONS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    output version information and exit
+
+OPTIONS:
+    -e, --expression <expression>    evaluate expression
+    -f, --file <file>                evaluate contents of file
+```
+
+Let us see some practical examples.  
+
+1. `Evaluate (-5 + sqrt(25-16))/2`:
+    ```
+    -5 25 16 - v + 2 / p
+    ```
+    _`v` stands for square root and `p` stands for print_
+2. `Evaluate (0.5+0.9)/3^4`
+    ```
+    0.5 0.9 + 3 4 ^ p
+    ```
+3. `Evalute 10 + 5 inline`:
+    ```bash
+    $> dc -e "10 5 +"
+    ```
+4. `Evaluate a file full expressions`:
+    ```bash
+    $> cat foo
+        5 5 + 
+        2 d * v 
+        f
+    $> dc -f ./foo
+    ```
+
+## Man page
+_TODO:_
+
+
+
+## Features
+As stated above, this tool only supports a proper subset of 
+original UNIX's `dc`. Here a complete list:
+| Command |        Meaning        |
+|:-------:|:---------------------:|
+|   `+`   |        Addition       |
+|   `-`   |      Subtraction      |
+|   `*`   |     Multiplication    |
+|   `/`   |        Division       |
+|   `%`   |         Modulo        |
+|   `^`   |      Exponential      |
+|   `v`   |      Square root      |
+|   `c`   |      Clear stack      |
+|   `d`   | Duplicate top element |
+|   `p`   |   Print top element   |
+|   `f`   |    Dump whole stack   |
+
+This `dc` implementation has a virtually endless stack, supports 
+floating point arithmetic(standard IEE754 double precision),
+inline expression parsing and file parsing.
+
+## Uni test
+TODO:
+
+
+## License
+This product is licensed under 
+[GPLv3](https://choosealicense.com/licenses/gpl-3.0/). You will obtain a copy 
+of this license by cloning this repository.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,227 @@
-use std::io;
+extern crate clap;
+const DC_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-// TODO: Command line parser support
-// TODO: Limit/setup decimal digits
+// Dependencies
+use std::io;
+use std::fs::File;
+use std::io::prelude::*;
+use clap::{Arg, App};
+
+// Takes expression and stack vector, then
+// Evaluate expression and push result to the stack
+fn evaluate_expression(expr: Vec<&str>, stack: &mut Vec<f64>) {
+    // For each item, match available operations
+    for token in expr {
+        match token {
+            // Math options
+            "+" => { // ADDITION
+                // Check if stack has enough items
+                if stack.len() < 2 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops two elements, sum them and push the result back to the stack
+                let sum = stack.pop().unwrap() + stack.pop().unwrap();
+                stack.push(sum);
+            },
+
+            "-" => { // SUBTRACTION
+                // Check if stack has enough items
+                if stack.len() < 2 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops two elements, subtract them and push the result back to the stack
+                let sub = -(stack.pop().unwrap() - stack.pop().unwrap());
+                stack.push(sub);
+            },
+
+            "*" => { // MULTIPLICATION
+                // Check if stack has enough items
+                if stack.len() < 2 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops two elements, multiply them and push the result back to the stack
+                let mul = stack.pop().unwrap() * stack.pop().unwrap();
+                stack.push(mul);
+            },
+
+            "/" => { // DIVISION
+                // Check if stack has enough items
+                if stack.len() < 2 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops two elements
+                let divisor = stack.pop().unwrap();
+                // Check if divisor is equal to zero
+                if divisor == 0.0 {
+                    println!("ERR: unable to divide by zero.");
+                    continue;
+                } else {
+                    let dividend = stack.pop().unwrap();
+                    stack.push(dividend / divisor);
+                }
+            },
+
+            "%" => { // MODULO
+                // Check if stack has enough items
+                if stack.len() < 2 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops two elements
+                let second_term = stack.pop().unwrap();
+                // Check if remainder is equal to zero
+                if second_term == 0.0 {
+                    println!("ERR: unable to divide by zero.");
+                    continue;
+                } else {
+                    let first_term = stack.pop().unwrap();
+                    stack.push(first_term % second_term);
+                }
+            },
+
+            "^" => { // EXPONENTIAL
+                // Check if stack has enough items
+                if stack.len() < 2 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops two elements
+                let exponent = stack.pop().unwrap();
+                let base = stack.pop().unwrap();
+                // Compute the exponent and push the result to the stack
+                stack.push(base.powf(exponent));
+            },
+
+            "v" => { // SQUARE ROOT
+                // Check if stack has enough items
+                if stack.len() < 1 {
+                    println!("ERR: stack is empty or either has too few elements.");
+                    continue;
+                }
+                // Pops one element
+                let argument = stack.pop().unwrap();
+                stack.push(argument.sqrt());
+            },
+            
+            // Stack options
+
+            "c" => { // CLEAR THE STACK
+                stack.clear();
+            },
+
+            "d" => { // DUPLICATE LAST ELEMENT OF THE STACK
+                // Check whether stack is empty
+                if stack.is_empty() {
+                    println!("ERR: stack is empty");
+                    continue;
+                }
+                let top_el = stack.last().cloned();
+                stack.push(top_el.unwrap());
+            },
+
+            "p" => { // PRINT TOP ELEMENT OF THE STACK
+                let head = stack.last().unwrap();
+                if head.fract() == 0.0 { // If number is integer
+                    println!("{0}", head); // Print without any rounding
+                } else {
+                    println!("{0:.4}", head); // Print with radix = 4
+                }
+            },
+
+            "f" => { // PRINT ENTIRE STACK
+                for item in stack.clone() {
+                    println!("{0}", item); // Print without rounding
+                }
+            },
+
+            // Default case
+            number => { // Handle all other options
+                // Check if whether is a number
+                match number.parse::<f64>() {
+                    Ok(_) => stack.push(number.parse::<f64>().unwrap()),
+                    Err(_) => println!("ERR: unrecognized option."),
+                }
+            }
+        }
+    }
+}
 
 fn main(){
     let mut user_input = String::new(); // Raw user input
     let mut stack_vec: Vec<f64> = Vec::new(); // Emulate a stack for Reverse Polish Notation
 
+    // Handle command line arguments
+    let matches = App::new("dc - RPN Desk calculator")
+                        .version("0.1.0")
+                        .author("Marco C. <ceticamarco@gmail.com>")
+                        .about("UNIX's dc clone")
+                        .arg(Arg::with_name("expression")
+                            .short("e")
+                            .long("expression")
+                            .required(false)
+                            .takes_value(true)
+                            .help("evaluate expression")
+                        ).arg(Arg::with_name("file")
+                            .short("f")
+                            .long("file")
+                            .required(false)
+                            .takes_value(true)
+                            .help("evaluate contents of file")
+                        ).arg(Arg::with_name("version")
+                        .short("V")
+                        .long("version")
+                        .required(false)
+                        .help("output version information and exit")
+                        ).get_matches();
+
+    // Handle command line parameters if required
+    if matches.is_present("expression") { // Inline expression option
+        let expression = matches.value_of("expression").unwrap(); // Retrieve parameter
+        // Tokenize expression
+        let tokens: Vec<&str> = expression.trim().split(' ').collect();
+        // Evaluate expression
+        evaluate_expression(tokens, &mut stack_vec);
+        // There's nothing left to do
+        std::process::exit(0);
+    }
+
+    if matches.is_present("file") { // Evaluate file option
+        let file_path = matches.value_of("file"); // Retrieve parameter
+        // Open file
+        let mut file_stream = match File::open(&file_path.unwrap()) {
+            Ok(file_stream) => file_stream,
+            Err(err) => panic!("Unable to open file \"{0}\": \"{1}\"", &file_path.unwrap(), err),
+        };
+        // Read file's content
+        let mut file_buffer = String::new();
+        match file_stream.read_to_string(&mut file_buffer) {
+            Ok(_) => (),
+            Err(err) => panic!("Unable to read from file \"{0}\": \"{1}\"", file_path.unwrap(), err),
+        }
+
+        // Tokenize buffer
+        file_buffer.retain(|c| {c != '\n'});
+        let tokens: Vec<&str> = file_buffer.split(' ').collect();
+        // Evaluate buffer
+        evaluate_expression(tokens, &mut stack_vec);
+        // There's nothing left to do
+        std::process::exit(0);
+    }
+
+    if matches.is_present("version") {
+        println!(
+            "dc - RPN Desk Calculator {version}\n\
+            This tool is NOT part of the GNU coreutils.\n\
+            Developed by Marco C.(<ceticamarco@gmail.com>) (c) 2021", 
+            version=DC_VERSION);
+        std::process::exit(0);
+    }
+
+    // Otherwise, just read from standard input
     loop {
         // Read from stdin
         io::stdin()
@@ -16,134 +231,9 @@ fn main(){
         // Remove newline and remove whitespace
         let tokens: Vec<&str> = user_input.trim().split(' ').collect();
 
-        // For each item, match available operations
-        for token in tokens {
-            match token {
-                // Math options
-                "+" => { // ADDITION
-                    // Check if stack has enough items
-                    if stack_vec.len() < 2 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops two elements, sum them and push the result back to the stack
-                    let sum = stack_vec.pop().unwrap() + stack_vec.pop().unwrap();
-                    stack_vec.push(sum);
-                },
+        // Evaluate expression
+        evaluate_expression(tokens, &mut stack_vec); // We borrow stack, since we need to reuse it
 
-                "-" => { // SUBTRACTION
-                    // Check if stack has enough items
-                    if stack_vec.len() < 2 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops two elements, subtract them and push the result back to the stack
-                    let sub = -(stack_vec.pop().unwrap() - stack_vec.pop().unwrap());
-                    stack_vec.push(sub);
-                },
-
-                "*" => { // MULTIPLICATION
-                    // Check if stack has enough items
-                    if stack_vec.len() < 2 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops two elements, multiply them and push the result back to the stack
-                    let mul = stack_vec.pop().unwrap() * stack_vec.pop().unwrap();
-                    stack_vec.push(mul);
-                },
-
-                "/" => { // DIVISION
-                    // Check if stack has enough items
-                    if stack_vec.len() < 2 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops two elements
-                    let divisor = stack_vec.pop().unwrap();
-                    // Check if divisor is equal to zero
-                    if divisor == 0.0 {
-                        println!("ERR: unable to divide by zero.");
-                        continue;
-                    } else {
-                        let dividend = stack_vec.pop().unwrap();
-                        stack_vec.push(dividend / divisor);
-                    }
-                },
-
-                "%" => { // MODULO
-                    // Check if stack has enough items
-                    if stack_vec.len() < 2 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops two elements
-                    let second_term = stack_vec.pop().unwrap();
-                    // Check if remainder is equal to zero
-                    if second_term == 0.0 {
-                        println!("ERR: unable to divide by zero.");
-                        continue;
-                    } else {
-                        let first_term = stack_vec.pop().unwrap();
-                        stack_vec.push(first_term % second_term);
-                    }
-                },
-
-                "^" => { // EXPONENTIAL
-                    // Check if stack has enough items
-                    if stack_vec.len() < 2 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops two elements
-                    let exponent = stack_vec.pop().unwrap();
-                    let base = stack_vec.pop().unwrap();
-                    // Compute the exponent and push the result to the stack
-                    stack_vec.push(base.powf(exponent));
-                },
-
-                "v" => { // SQUARE ROOT
-                    // Check if stack has enough items
-                    if stack_vec.len() < 1 {
-                        println!("ERR: stack is empty or either has too few elements.");
-                        continue;
-                    }
-                    // Pops one element
-                    let argument = stack_vec.pop().unwrap();
-                    stack_vec.push(argument.sqrt());
-                },
-                
-                // Stack options
-
-                "c" => { // CLEAR THE STACK
-                    stack_vec.clear();
-                },
-
-                "d" => { // DUPLICATE LAST ELEMENT OF THE STACK
-                    let top_el = stack_vec.last().cloned();
-                    stack_vec.push(top_el.unwrap());
-                },
-
-                "p" => { // PRINT TOP ELEMENT OF THE STACK
-                    println!("{0}", stack_vec.last().unwrap());
-                },
-
-                "f" => { // PRINT ENTIRE STACK
-                    for item in &stack_vec {
-                        println!("{0}", item);
-                    }
-                },
-
-                // Default case
-                number => { // Handle all other options
-                    // Check if whether is a number
-                    match number.parse::<f64>() {
-                        Ok(_) => stack_vec.push(number.parse::<f64>().unwrap()),
-                        Err(_) => println!("ERR: unrecognized option."),
-                    }
-                }
-            }
-        }
         // Clear user input string
         user_input.clear();
     }
